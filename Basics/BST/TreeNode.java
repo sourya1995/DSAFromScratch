9cc4197b243a1cacdac9e05214dd7e8fae05ed2a
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2025-10-02 07:00:30 +0530

    feat: Add TreeNode.java to the BST structure

---
+++ Basics/BST/TreeNode.java
@@ -0,0 +1,11 @@
+package Basics.BST;
+
+public class TreeNode {
+    int data;
+    TreeNode left, right;
+
+    public TreeNode(int item) {
+        this.data = item;
+        this.left = this.right = null;
+    }
+}
