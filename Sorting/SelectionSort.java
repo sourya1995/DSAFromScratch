1096cdc17a488a8b4167cdbc1795d942a515d4f7
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2025-10-04 00:04:15 +0530

    feat: Add selection sort algorithm
    This commit introduces the selection sort algorithm. The implementation is in Java and is located in the Sorting directory.

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        // Traverse through all array elements
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+}
