cbd39f4ec43b04f3f09c603d965b0bd000e53038
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2025-10-08 21:31:18 +0530

    feat: Add solution for finding minimum in a rotated sorted array

---
+++ BinarySearch/FindMinimumInARotatedSortedArray.java
@@ -0,0 +1,44 @@
+package BinarySearch;
+
+/* Leetcode 153 - Find Minimum in Rotated Sorted Array */ *
+
+public class FindMinimumInARotatedSortedArray {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int result = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] < nums[result]){
+                result = mid;
+            }
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return nums[result];
+    }// this is a meh solution
+
+    class Solution {
+        public int findMin(int[] nums) {
+            int left = 0;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] > nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return nums[left];
+        }
+    } // this is a better solution
+
+} 
