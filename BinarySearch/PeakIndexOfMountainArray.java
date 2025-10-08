e1e7f8cd8f8f4d85532b18fb1fa64e39b7b7d8da
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2025-10-08 21:50:46 +0530

    feat: Add solution for finding peak index of a mountain array

---
+++ BinarySearch/PeakIndexOfMountainArray.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+public class PeakIndexOfMountainArray {
+    public int peakIndexInMountainArray(int[] arr){
+        int left = 0;
+        int right = arr.length - 1;
+        int boundaryIndex = -1;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(arr[mid] > arr[mid + 1]){
+                boundaryIndex = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return boundaryIndex;
+    }
+}
